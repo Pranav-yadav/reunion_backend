347158d888349a71ddefa51ee2ac152c19ef639e
Pranav Yadav <Pranavyadav3912@gmail.com>
2023-01-19 11:45:28 +0000

    setup and run docker script - npm command: `npm docker`

--- setup_and_start.sh
+++ setup_and_start.sh
@@ -8,12 +8,19 @@ docker build -t backend .
 # run docker container
 echo "[2/2] Running docker container."
 docker run -d -p 3000:3000 --name backend backend:latest
+# echo ""
 
 # check if container is running
-docker ps -a
+# docker ps -a
+# echo ""
 
 # check if everything is running without errors with logs
-docker logs backend
+# docker logs backend
 
-# visit the webapp
-echo "✅ Backend apis running at http://localhost:3000"
+# echo "✅ Backend apis running at http://localhost:3000"
+# echo ""
+
+# docker ps -a && docker images
+# docker stop backend
+# docker rm backend
+# docker rmi backend
---
+++ start.sh
@@ -0,0 +1,5 @@
+# Description: Start the mongo server
+chmod +x ./init-mongo.sh && ./init-mongo.sh
+
+# Start express server
+npm start
